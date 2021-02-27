4a8abeb847ef3ca13d04dc9dc00423d28e2fb955
AdamJ335 <adamj335@gmail.com>
2021-02-27 14:36:31 +0000

    Basic Level Generation
    Need to keep within box, need to connect, need to not continuously generate

--- Evac/Assets/Scripts/LevelGeneration.cs
+++ Evac/Assets/Scripts/LevelGeneration.cs
@@ -5,17 +5,55 @@ using UnityEngine;
 public class LevelGeneration : MonoBehaviour
 {
     public Transform[] startingPositions;
+    public GameObject[] rooms;
+    private int direction;
+    public float moveAmount;
+    private float timeBtwRoom;
+    private float startTimeBtwRoom = 0.25f;
     // Start is called before the first frame update
     void Start()
     {
         int randStartingPos = Random.Range(0, startingPositions.Length);
         transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
 
+        direction = Random.Range(1, 6);
+
+    }
+    private void Move()
+    {
+        if (direction == 1 || direction == 2)
+        { //Move RIGHT
+            Vector3 newPos = new Vector3(transform.position.x + moveAmount, transform.position.y, 0);
+            transform.position = newPos;
+        }
+        else if (direction == 3 || direction == 4)
+        { //Move LEFT
+            Vector3 newPos = new Vector3(transform.position.x - moveAmount, transform.position.y, 0);
+            transform.position = newPos;
+        }
+        else if (direction == 5)
+        { //Move DOWN
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y - moveAmount, 0);
+            transform.position = newPos;
+        }
+
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+        direction = Random.Range(1, 6);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timeBtwRoom <= 0)
+        {
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime;
+        }
 
     }
 }
