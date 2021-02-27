be855e03644159bf5f95b7020d88e8894500d2ea
AdamJ335 <adamj335@gmail.com>
2021-02-27 14:01:02 +0000

    Generated Spawning Rooms

---
+++ Evac/Assets/Scripts/LevelGeneration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour
+{
+    public Transform[] startingPositions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int randStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
