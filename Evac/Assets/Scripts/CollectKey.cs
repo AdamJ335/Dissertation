fc288c50d284f6b3a7ee937b8603234d57099656
AdamJ335 <adamj335@gmail.com>
2021-04-13 12:55:50 +0100

    initialize waypoints

--- Evac/Assets/Scripts/CollectKey.cs
+++ Evac/Assets/Scripts/CollectKey.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class CollectKey : MonoBehaviour
 {
     public AudioSource collectSound;
+    public static bool isKeyCollected = false;
 
     void OnTriggerEnter(Collider other)
     {
         collectSound.Play();
         ScoringSystem.score -= 1;
+        isKeyCollected = true;
         Destroy(gameObject);
+
     }
 }
