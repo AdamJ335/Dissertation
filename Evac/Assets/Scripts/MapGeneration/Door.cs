cca7fda7cd1cafae7f431dcf75196d6009617f30
AdamJ335 <adamj335@gmail.com>
2021-04-21 15:00:54 +0100

    Full map generation complete

--- Evac/Assets/Scripts/MapGeneration/Door.cs
+++ Evac/Assets/Scripts/MapGeneration/Door.cs
@@ -10,4 +10,5 @@ public class Door : MonoBehaviour
     }
 
     public DoorType doorType;
+
 }
---
+++ Evac/Assets/Scripts/MapGeneration/DoorMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour
+{
+    public string doorType;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (doorType == "right")
+            {
+                //sends player to room on the right
+                other.transform.position += new Vector3(3, 0, 0);
+            }
+            else if (doorType == "left")
+            {
+                //sends player to room on the left
+                other.transform.position += new Vector3(-3, 0, 0);
+            }
+            else if (doorType == "up")
+            {   //sends player to room above
+                other.transform.position += new Vector3(0, 0, 3);
+            }
+            else if (doorType == "down")
+            {   //sends player to room below
+                other.transform.position += new Vector3(0, 0, -3);
+            }
+        }
+    }
+}
