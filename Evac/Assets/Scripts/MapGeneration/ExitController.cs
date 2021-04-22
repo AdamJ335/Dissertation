185e8488ae1de3fe6314d75d6b5d76c7f0c46783
AdamJ335 <adamj335@gmail.com>
2021-04-22 12:08:34 +0100

    Added basic Save to txt

---
+++ Evac/Assets/Scripts/MapGeneration/ExitController.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            Save();
+        }
+    }
+
+    private void Save()
+    {
+        //Save
+        File.WriteAllText("C:/Users/Adam Jennings/Desktop/Projects/Dissertation/Evac/save.txt", "test");
+    }
+}
